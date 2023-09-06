e517c13c490644fe4c6f03af3d0e742993dd6457
Dtikoli <tikolidavid@yahoo.com>
2023-09-06 16:25:04 -0700

    Resolving bugs

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -16,7 +16,7 @@ def do_pack():
     timestamp = strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
-        local("tar -czvf versions/web_static_}.tgz web_static/"
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
               .format(timestamp))
 
         return "versions/web_static_{}.tgz".format(timestamp)
