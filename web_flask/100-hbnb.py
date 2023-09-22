30b328c3a9b16c400b6bc56694ba5760e5c30d15
Dtikoli <tikolidavid@yahoo.com>
2023-09-22 11:26:25 -0700

    HBNB is alive!

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Script that runs an app with Flask framework """
+""" A script that starts a Flask web application """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -16,17 +16,17 @@ def teardown_session(exception):
     storage.close()
 
 
-@app.route('/hbnb/', strict_slashes=False)
+@app.route("/hbnb/", strict_slashes=False)
 def display_html():
-    """ Function called with /states route """
+    """ Function called with route /hbnb """
     states = storage.all(State)
     amenities = storage.all(Amenity)
     places = storage.all(Place)
 
-    return render_template('100-hbnb.html',
+    return render_template("100-hbnb.html",
                            states=states.values(),
                            amenities=amenities.values(),
                            places=places.values())
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
