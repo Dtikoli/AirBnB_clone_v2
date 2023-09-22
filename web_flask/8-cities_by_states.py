94d8eb361ad24739db8a30bc81f85ced70ec83dd
Dtikoli <tikolidavid@yahoo.com>
2023-09-22 06:49:04 -0700

    Cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Script that runs an app with Flask framework """
+""" A script that starts a Flask web application """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -15,13 +15,13 @@ def teardown_session(exception):
     storage.close()
 
 
-@app.route('/cities_by_states', strict_slashes=False)
+@app.route("/cities_by_states", strict_slashes=False)
 def display_html():
     """ Function called with /states_list route """
     states = storage.all(State)
-    return render_template('8-cities_by_states.html',
+    return render_template("8-cities_by_states.html",
                            Table="States",
                            states=states)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
