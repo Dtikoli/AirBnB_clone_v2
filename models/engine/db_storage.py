f3d8641630d74b11199188f1a108261a6e45fb0f
Dtikoli <tikolidavid@yahoo.com>
2023-09-22 06:15:38 -0700

    Improve engines

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module for Database storage"""
+""" Module for Database storage """
 from models.base_model import BaseModel, Base
 from models.amenity import Amenity
 from models.city import City
