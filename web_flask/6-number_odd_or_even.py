c29e339622190c09e4f16a331c4647f43f6324f7
Dtikoli <tikolidavid@yahoo.com>
2023-09-22 06:01:09 -0700

    Odd or even?

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,54 +1,55 @@
 #!/usr/bin/python3
-""" Script that runs an app with Flask framework """
+""" A script that starts a Flask web application """
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """ Function called with / route """
-    return 'Hello HBNB!'
+    """ Function called with route / """
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """ Function called with /hbnb route """
-    return 'HBNB'
+    """ Function called with route /hbnb """
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route("/c/<text>", strict_slashes=False)
 def c_text(text):
-    """ Function called with /c/<text> route """
-    return 'C %s' % text.replace('_', ' ')
+    """ Function called with route /c/<text> """
+    text = text.replace("_", " ")
+    return f"C {text}"
 
 
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_text(text='is cool'):
-    """ Function called with /python/<text> route """
-    if text is not 'is cool':
-        text = text.replace('_', ' ')
-    return 'Python %s' % text
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text="is cool"):
+    """ Function called with route /python/<text> """
+    text = text.replace("_", " ")
+    return f"Python {text}"
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
+@app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
-    """ Function called with /number/<n> route """
-    return "%d is a number" % n
+    """ Function called with route /number/<n> """
+    return f"{n} is a number"
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
+@app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
-    """ Function called with /number_template/<n> route """
-    return render_template('5-number.html', number=n)
+    """ Function called with route /number_template/<n> """
+    return render_template("5-number.html", number=n)
 
 
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_odd_or_even(n):
-    """ Function called with /number_template/<n> route """
-    return render_template('6-number_odd_or_even.html', number=n)
+    """ Function called with route /number_template/<n> """
+    return render_template("6-number_odd_or_even.html", number=n)
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
