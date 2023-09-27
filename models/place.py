e5f1b6877dbcab1c3c9b1521e785182f406be9d2
Dtikoli <tikolidavid@yahoo.com>
2023-09-27 04:05:27 -0700

    Fix in place module

--- models/place.py
+++ models/place.py
@@ -4,8 +4,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
+from models.amenity import Amenity
+from models.review import Review
 from os import getenv
-import models
+import shlex
 
 
 place_amenity = Table("place_amenity", Base.metadata,
